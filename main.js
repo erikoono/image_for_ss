41c77bc6ed328524f4c59680f3786597a3c0f7fd
erikoono <luvricokj8@gmail.com>
2021-04-13 08:20:36 +0900

    create

---
+++ main.js
@@ -0,0 +1,53 @@
+function insertman() {
+
+  //サンプルリストからURLを取得し、配列に格納する
+  //const parent_ss_id = "19f68xiWutOv4bRv7YppXuzU8bi0Mcmq6sUS-01DDXAE" //会社
+  const parent_ss_id = "1moCUxU7O7-3j15arjCN6roY4wyl3Rn4BEATuTEYQQLI"   //家
+  const parent_ss = SpreadsheetApp.openById(parent_ss_id)
+  const parent_sh = parent_ss.getSheetByName("フォームの回答 1")
+  const list_number = parent_sh.getRange(2, 3, parent_sh.getLastRow() - 1).getValues()
+  const list_name = parent_sh.getRange(2, 4, parent_sh.getLastRow() - 1).getValues()
+  const list_bank = parent_sh.getRange(2, 5, parent_sh.getLastRow() - 1).getValues()
+  const list_shiten = parent_sh.getRange(2, 7, parent_sh.getLastRow() - 1).getValues()
+  const list_kouza = parent_sh.getRange(2, 8, parent_sh.getLastRow() - 1).getValues()
+  const list_meigi = parent_sh.getRange(2, 9, parent_sh.getLastRow() - 1).getValues()
+  const list_url = parent_sh.getRange(2, 10, parent_sh.getLastRow() - 1).getValues()
+  const header_list = ["社員番号", "氏名", "金融機関名", "支店番号", "口座番号", "口座名義"]
+  const count_list = list_number.length
+  let ss_count
+  let ss
+  let sheet
+  const r = Array.apply(null, new Array(count_list)).map(function(_, i) {
+    
+    console.log("ループ回数：".concat(i))
+
+    //100件ごとにスプレッドシートの作成
+    if (i == 0 || i % 100 == 0){
+      ss_count = i == 0 ? ss_count = 1 : i % 100 == 0 ? ss_count = i / 100 + 1 : ss_count = ss_count
+      ss = SpreadsheetApp.openById(SpreadsheetApp.create("ss_".concat(ss_count)).getId())
+      sheet = ss.getSheets()[0]
+    } else {
+      //スプレッドシートに新しいシートを作成
+      sheet = ss.insertSheet()
+    }
+
+    //ドライブから画像を取得
+    let image = DriveApp.getFileById(list_url[i][0].indexOf(',') != -1 ? list_url[i][0].substring(0, list_url[i][0].indexOf(",")).replace("https://drive.google.com/open?id=", "").replace("https://drive.google.com/file/d/", "").replace("/view?usp=sharing", "") : list_url[i][0].indexOf("\n") != -1 ? list_url[i][0].substring(0, list_url[i][0].indexOf("\n")).replace("https://drive.google.com/open?id=", "").replace("https://drive.google.com/file/d/", "").replace("/view?usp=sharing", "") : list_url[i][0].replace("https://drive.google.com/open?id=", "").replace("https://drive.google.com/file/d/", "").replace("/view?usp=sharing", ""))
+    if (image.getBlob().getContentType() == "image/jpeg") {
+      //画像を挿入
+      const values = [
+        header_list,
+        [list_number[i][0],
+          list_name[i][0],
+          list_bank[i][0],
+          list_shiten[i][0],
+          list_kouza[i][0],
+          list_meigi[i][0]
+        ]
+      ]
+      sheet.getRange(1, 1, 2, 6).setNumberFormat('@').setValues(values)
+      sheet.insertImage(ImgApp.doResize(image.getId(), 1000).blob, 1, 4)
+    }
+
+  })
+}
